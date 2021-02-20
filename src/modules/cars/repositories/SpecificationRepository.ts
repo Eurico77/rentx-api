26a596bb131836bb30a1820ec58bc878c27ccd0e
Eurico Magalhães <euricomagalhaes61@gmail.com>
2021-02-20 07:21:54 -0300

    ajustes diversos

--- src/modules/cars/repositories/SpecificationRepository.ts
+++ src/modules/cars/repositories/SpecificationRepository.ts
@@ -1,8 +1,8 @@
-import { Specification } from "../models/Specification";
+import { Specification } from '../models/Specification';
 import {
   ISpecificationsRepository,
   ICreateSpecificationsDTO,
-} from "./ISpecificationsRepository";
+} from './implementations/ISpecificationsRepository';
 
 class SpecificationsRepository implements ISpecificationsRepository {
   private specifications: Specification[];
