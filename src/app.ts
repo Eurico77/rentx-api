1c53faa87f16a4af235c9b96face62e4bb9fb42f
Eurico Magalhães <euricomagalhaes61@gmail.com>
2021-03-28 01:49:38 -0300

    add rotas ao app

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 import express from 'express';
+import { routes } from './routes';
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
+app.use(routes)
 
 
-
-
-export default app
+export { app }
