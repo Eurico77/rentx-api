2dce4104084b67520cfdfef7f0f716394b48fdb6
Eurico Magalhães <euricomagalhaes61@gmail.com>
2021-03-31 14:32:23 -0300

    ajustes da rotas

--- src/routes/categories/categories.routes.ts
+++ src/routes/categories/categories.routes.ts
@@ -1,15 +1,22 @@
 import { Router } from 'express';
 import { CategoriesRepository } from '../../repositories/categories/CategoriesRepository';
+import { CreateCategoryService } from '../../services/categories/CreateCategoryService';
 
 const categoriesRouter = Router();
 const categoriesRepository = new CategoriesRepository()
 
-
 categoriesRouter.post('/', (request, response) => {
     const { name, description } = request.body;
+    
+    const createCategoryService = new CreateCategoryService(categoriesRepository)
+    createCategoryService.execute({ name, description })
 
-    categoriesRepository.create({ name, description })
     return response.status(201).send()
+})
+
+categoriesRouter.get('/', (request, response) => {
+    const all = categoriesRepository.index();
+    return response.json({categories :all})
 })
 
 
