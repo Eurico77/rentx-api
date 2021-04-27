73092f53d41e39b478c4b53a94c36d05edf65c09
Eurico Magalhães <euricomagalhaes61@gmail.com>
2021-04-27 00:38:59 -0300

    adjustments app

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
-import express from 'express';
-import { routes } from './routes';
+import express from "express";
+
+import { routes } from "./routes";
 
 const app = express();
 app.use(express.json());
-app.use(routes)
+app.use(routes);
 
-
-export { app }
+export { app };
