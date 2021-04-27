f297152f56af17192d57826ca9ca9763076b731e
Eurico Magalhães <euricomagalhaes61@gmail.com>
2021-04-26 23:28:13 -0300

    adjustments structural pastes

---
+++ src/models/Category.ts
@@ -0,0 +1,16 @@
+import { v4 as uuidV4 } from "uuid";
+
+class Category {
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
+
+  id?: string;
+  name: string;
+  description: string;
+  created_at: Date;
+}
+
+export { Category };
---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,33 @@
+import { Category } from "../models/Category";
+import {
+  ICategoryRepository,
+  ICreateCategoryDTO,
+} from "./ICategoriesRepository";
+
+class CategoriesRepository implements ICategoryRepository {
+  private categories: Category[];
+
+  constructor() {
+    this.categories = [];
+  }
+  create({ name, description }: ICreateCategoryDTO): void {
+    const category = new Category();
+
+    Object.assign(category, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+    this.categories.push(category);
+  }
+
+  index(): Array<Category> {
+    return this.categories;
+  }
+  findByName(name): Category {
+    const category = this.categories.find((category) => category.name === name);
+    return category;
+  }
+}
+
+export { CategoriesRepository };
