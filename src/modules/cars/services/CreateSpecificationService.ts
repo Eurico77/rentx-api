38498a7d294e21d83d9105b7d719b901ca786fb1
Eurico Magalhães <euricomagalhaes61@gmail.com>
2021-04-27 00:40:36 -0300

    add ServiceSpecification

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,6 +1,14 @@
+import { ISpecificationsRepository } from "../repositories/ISpecificationsRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
 class CreateSpecificationService {
-  execute(): void {
-    return console.log("todo");
+  constructor(private specificationsRepository: ISpecificationsRepository) {}
+  execute({ name, description }: IRequest): void {
+    this.specificationsRepository.create({ name, description });
   }
 }
 
