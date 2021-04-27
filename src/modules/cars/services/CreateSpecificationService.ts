823b6e8915984740aace3bf5ff056af37a8b889f
Eurico Magalhães <euricomagalhaes61@gmail.com>
2021-04-27 01:30:01 -0300

    adjustamets

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -8,6 +8,12 @@ interface IRequest {
 class CreateSpecificationService {
   constructor(private specificationsRepository: ISpecificationsRepository) {}
   execute({ name, description }: IRequest): void {
+    const specificationAlreadyExists = this.specificationsRepository.findByName(
+      name
+    );
+
+    if (specificationAlreadyExists)
+      throw new Error("Specification already exists");
     this.specificationsRepository.create({ name, description });
   }
 }
