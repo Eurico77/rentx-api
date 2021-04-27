321003b43fefe8dc759e9db913c0703d450c973e
Eurico Magalhães <euricomagalhaes61@gmail.com>
2021-04-27 00:40:58 -0300

    adjustament routes

--- src/routes/categories/categories.routes.ts
+++ src/routes/categories/categories.routes.ts
@@ -1,23 +1,23 @@
-// import { Router } from "express";
+import { Router } from "express";
 
-// import { CategoriesRepository } from "../../modules/cars/repositories/categories/CategoriesRepository";
-// import { CreateCategoryService } from "../../modules/cars/services/categories/CreateCategoryService";
+import { CategoriesRepository } from "../../modules/cars/repositories/CategoriesRepository";
+import { CreateCategoryService } from "../../modules/cars/services/CreateCategoryService";
 
-// const categoriesRouter = Router();
-// const categoriesRepository = new CategoriesRepository();
+const categoriesRouter = Router();
+const categoriesRepository = new CategoriesRepository();
 
-// categoriesRouter.post("/", (request, response) => {
-//   const { name, description } = request.body;
+categoriesRouter.post("/", (request, response) => {
+  const { name, description } = request.body;
 
-//   const createCategoryService = new CreateCategoryService(categoriesRepository);
-//   createCategoryService.execute({ name, description });
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
+  createCategoryService.execute({ name, description });
 
-//   return response.status(201).send();
-// });
+  return response.status(201).send();
+});
 
-// categoriesRouter.get("/", (request, response) => {
-//   const all = categoriesRepository.index();
-//   return response.json({ categories: all });
-// });
+categoriesRouter.get("/", (request, response) => {
+  const all = categoriesRepository.index();
+  return response.json({ categories: all });
+});
 
-// export { categoriesRouter };
+export { categoriesRouter };
