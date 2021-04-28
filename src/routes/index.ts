5857cfe60bcab95e7034dab14f16b3259173ba31
Eurico Magalhães <euricomagalhaes61@gmail.com>
2021-04-27 22:54:20 -0300

    ajustes de identacao

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,10 @@
-import { Router } from "express";
+import { Router } from 'express';
 
-import { categoriesRoutes } from "./categories.routes";
-import { specificationsRoutes } from "./specifications.routes";
+import { categoriesRoutes } from './categories.routes';
+import { specificationsRoutes } from './specifications.routes';
 
 const routes = Router();
-routes.use("/categories", categoriesRoutes);
-routes.use("/specifications", specificationsRoutes);
+routes.use('/categories', categoriesRoutes);
+routes.use('/specifications', specificationsRoutes);
 
 export { routes };
