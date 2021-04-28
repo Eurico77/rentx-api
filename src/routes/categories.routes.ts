5e584b45487514a49663f446e55b79d054a5c5cd
Eurico Magalhães <euricomagalhaes61@gmail.com>
2021-04-27 22:55:38 -0300

    add route getAll category

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,11 +1,16 @@
 import { Router } from 'express';
 
 import { createCategoryController } from '../modules/cars/useCases/createCategory/factory/CreateCategory';
+import { listCategoriesController } from '../modules/cars/useCases/listCategory/factory/ListCategory';
 
 const categoriesRoutes = Router();
 
 categoriesRoutes.post('/', (request, response) => {
   return createCategoryController.handler(request, response);
+});
+
+categoriesRoutes.get('/', (request, response) => {
+  return listCategoriesController.handler(request, response);
 });
 
 export { categoriesRoutes };
