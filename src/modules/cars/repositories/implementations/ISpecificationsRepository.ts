d8d8da363446df89f1363c53c6105f73f102bd4f
Eurico Magalhães <euricomagalhaes61@gmail.com>
2021-04-28 01:23:34 -0300

    adjustments of modules implementatios

---
+++ src/modules/cars/repositories/implementations/ISpecificationsRepository.ts
@@ -0,0 +1,13 @@
+import { Specification } from "../models/Specification";
+
+interface ICreateSpecificationsDTO {
+  name: string;
+  description: string;
+}
+
+interface ISpecificationsRepository {
+  create({ description, name }: ICreateSpecificationsDTO);
+  findByName(name: string): Specification;
+}
+
+export { ISpecificationsRepository, ICreateSpecificationsDTO };
