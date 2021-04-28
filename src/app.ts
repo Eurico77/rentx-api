c759bf566055ab4cde8a7c6c00751f57ecb2c69f
Eurico Magalhães <euricomagalhaes61@gmail.com>
2021-04-27 22:53:46 -0300

    ajustes de identacao

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
-import express from "express";
+import express from 'express';
 
-import { routes } from "./routes";
+import { routes } from './routes';
 
 const app = express();
 app.use(express.json());
