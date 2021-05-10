c46641404ffaaf6eb2586181c0156131ae9dece3
eurico77 <euricomagalhaes61@gmail.com>
2021-05-10 00:59:00 -0300

    adjustments

---
+++ src/modules/cars/repositories/implementations/SpecificationRepository.ts
@@ -0,0 +1,30 @@
+import { getRepository, Repository } from 'typeorm';
+
+import { Specification } from '../../entities/Specification';
+import {
+  ISpecificationsRepository,
+  ICreateSpecificationsDTO,
+} from '../ISpecificationsRepository';
+
+class SpecificationsRepository implements ISpecificationsRepository {
+  private repository: Repository<Specification>;
+  constructor() {
+    this.repository = getRepository(Specification);
+  }
+
+  async create({ description, name }: ICreateSpecificationsDTO): Promise<void> {
+    const specification = this.repository.create({
+      description,
+      name,
+    });
+
+    await this.repository.save(specification);
+  }
+
+  findByName(name: string): Promise<Specification> {
+    const specification = this.repository.findOne({ name });
+
+    return specification;
+  }
+}
+export { SpecificationsRepository };
