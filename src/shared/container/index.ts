cc7044928bd61b6afdcb515281282e5a18705eac
eurico77 <euricomagalhaes61@gmail.com>
2021-05-09 23:28:32 -0300

    refactorys

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,13 +2,14 @@ import { container } from 'tsyringe';
 
 import { CategoriesRepository } from '../../modules/cars/repositories/CategoriesRepository';
 import { ICategoryRepository } from '../../modules/cars/repositories/implementations/ICategoriesRepository';
+import { ISpecificationsRepository } from '../../modules/cars/repositories/implementations/ISpecificationsRepository';
 import { SpecificationsRepository } from '../../modules/cars/repositories/SpecificationRepository';
 
 container.registerSingleton<ICategoryRepository>(
   'CategoriesRepository',
   CategoriesRepository
 );
-container.registerSingleton<SpecificationsRepository>(
+container.registerSingleton<ISpecificationsRepository>(
   'SpecificationsRepository',
   SpecificationsRepository
 );
