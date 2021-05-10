cbfbe8bb883bfe10a027f29f73d581de6b63cf7a
eurico77 <euricomagalhaes61@gmail.com>
2021-05-09 22:40:25 -0300

    refactorys

--- src/modules/cars/entities/Category.ts
+++ src/modules/cars/entities/Category.ts
@@ -1,5 +1,7 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
 import { v4 as uuidV4 } from 'uuid';
 
+@Entity('categories')
 class Category {
   constructor() {
     if (!this.id) {
@@ -7,9 +9,16 @@ class Category {
     }
   }
 
+  @PrimaryColumn()
   id?: string;
+
+  @Column()
   name: string;
+
+  @Column()
   description: string;
+
+  @CreateDateColumn()
   created_at: Date;
 }
 
