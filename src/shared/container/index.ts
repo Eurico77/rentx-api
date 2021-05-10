90fd7fafdb291febbc4ce53daa2d2f0178e85c1f
eurico77 <euricomagalhaes61@gmail.com>
2021-05-10 00:59:23 -0300

    adjustments

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,9 +1,9 @@
 import { container } from 'tsyringe';
 
-import { CategoriesRepository } from '../../modules/cars/repositories/CategoriesRepository';
-import { ICategoryRepository } from '../../modules/cars/repositories/implementations/ICategoriesRepository';
-import { ISpecificationsRepository } from '../../modules/cars/repositories/implementations/ISpecificationsRepository';
-import { SpecificationsRepository } from '../../modules/cars/repositories/SpecificationRepository';
+import { ICategoryRepository } from '../../modules/cars/repositories/ICategoriesRepository';
+import { CategoriesRepository } from '../../modules/cars/repositories/implementations/CategoriesRepository';
+import { SpecificationsRepository } from '../../modules/cars/repositories/implementations/SpecificationRepository';
+import { ISpecificationsRepository } from '../../modules/cars/repositories/ISpecificationsRepository';
 
 container.registerSingleton<ICategoryRepository>(
   'CategoriesRepository',
