885150e97c84a733d050df9df1fc1e6d8a7af40d
eurico77 <euricomagalhaes61@gmail.com>
2021-05-09 22:41:08 -0300

    adjustments

--- src/modules/cars/useCases/importCategory/factory/CreateImportCategory.ts
+++ src/modules/cars/useCases/importCategory/factory/CreateImportCategory.ts
@@ -3,7 +3,7 @@ import { ImportCategoryController } from '../ImportCategoryController';
 import { ImportCategoryUseCase } from '../ImportCategoryUseCase';
 
 // usando singleton
-const categoriesRepository = CategoriesRepository.getInstance();
+const categoriesRepository = null;
 const importCategoryUseCase = new ImportCategoryUseCase(categoriesRepository);
 const importCategoryController = new ImportCategoryController(
   importCategoryUseCase
