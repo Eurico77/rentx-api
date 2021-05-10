0210ec5dddf4eeec6eeb139747145d0be84d07c5
eurico77 <euricomagalhaes61@gmail.com>
2021-05-10 00:59:21 -0300

    adjustments

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -2,7 +2,7 @@ import csvParse from 'csv-parse';
 import fs from 'fs';
 import { inject, injectable } from 'tsyringe';
 
-import { ICategoryRepository } from '../../repositories/implementations/ICategoriesRepository';
+import { ICategoryRepository } from '../../repositories/ICategoriesRepository';
 
 interface IImportCategory {
   name: string;
