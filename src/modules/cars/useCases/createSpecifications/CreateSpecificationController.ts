d2c165e33fe1ab41bb4416e934b43b023341f015
eurico77 <euricomagalhaes61@gmail.com>
2021-05-09 23:52:25 -0300

    refactorys specifications controller

--- src/modules/cars/useCases/createSpecifications/CreateSpecificationController.ts
+++ src/modules/cars/useCases/createSpecifications/CreateSpecificationController.ts
@@ -4,13 +4,15 @@ import { container } from 'tsyringe';
 import { CreateSpecificationUseCase } from './CreateSpecificationUseCase';
 
 class CreateSpecificationController {
-  handler(request: Request, response: Response): Response {
+  async handler(request: Request, response: Response): Promise<Response> {
     const { name, description } = request.body;
+
     const createSpecificationUseCase = container.resolve(
       CreateSpecificationUseCase
     );
 
-    createSpecificationUseCase.execute({ name, description });
+    await createSpecificationUseCase.execute({ name, description });
+
     return response.status(200).send();
   }
 }
