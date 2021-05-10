6099a7084a332c8e6789614a91a6a596b4219f17
eurico77 <euricomagalhaes61@gmail.com>
2021-05-10 00:59:17 -0300

    adjustments

--- src/modules/cars/useCases/createSpecifications/CreateSpecificationUseCase.ts
+++ src/modules/cars/useCases/createSpecifications/CreateSpecificationUseCase.ts
@@ -1,6 +1,6 @@
 import { inject, injectable } from 'tsyringe';
 
-import { ISpecificationsRepository } from '../../repositories/implementations/ISpecificationsRepository';
+import { ISpecificationsRepository } from '../../repositories/ISpecificationsRepository';
 
 interface IRequest {
   name: string;
