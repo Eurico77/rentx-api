3a24cbd0858a5ca8a9fa2cc7e3578a048bb1c29c
eurico77 <euricomagalhaes61@gmail.com>
2021-05-10 21:38:58 -0300

    feature authentication routes routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 
+import { authenticateRoutes } from './authenticate.routes';
 import { categoriesRoutes } from './categories.routes';
 import { specificationsRoutes } from './specifications.routes';
 import { usersRoutes } from './users.routes';
@@ -8,5 +9,6 @@ const routes = Router();
 routes.use('/categories', categoriesRoutes);
 routes.use('/specifications', specificationsRoutes);
 routes.use('/users', usersRoutes);
+routes.use(authenticateRoutes);
 
 export { routes };
