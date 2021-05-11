865863ee74016717f28e239681a20961f2425cd2
eurico77 <euricomagalhaes61@gmail.com>
2021-05-11 01:26:02 -0300

    feature add: avatar routes and sessions

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,23 +1,17 @@
 import { Router } from 'express';
 import multer from 'multer';
 
+import uploadConfig from '../config/uploads';
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 import { CreateUserController } from '../modules/accounts/useCases/createUser/CreateUserController';
-// import { UpdateUserAvatarController } from '@modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController';
-// import { ensureAuthenticated } from '@shared/middlewares/ensureAuthenticated';
+import { UpdateUserAvatarController } from '../modules/accounts/useCases/updateUserAvatar/UpdateUserAvaterController';
 
-// import uploadConfig from './config/upload';
-
-// const uploadAvatar = multer(uploadConfig.upload('./tmp/avatar'));
+const uploadAvatar = multer(uploadConfig.upload('./tmp/avatar'));
 const usersRoutes = Router();
 const createUserController = new CreateUserController();
-// const updateUserAvatarController = new UpdateUserAvatarController();
+const updateUserAvatarController = new UpdateUserAvatarController();
 
 usersRoutes.post('/', createUserController.handle);
-// usersRoutes.patch(
-//   '/avatar',
-//   ensureAuthenticated,
-//   uploadAvatar.single('avatar'),
-//   updateUserAvatarController.handle
-// );
+usersRoutes.patch('/avatar', ensureAuthenticated, uploadAvatar.single('avatar'), updateUserAvatarController.handle);
 
 export { usersRoutes };
