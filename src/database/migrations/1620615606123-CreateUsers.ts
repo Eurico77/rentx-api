56bb40aefdc11a198051b0f1a8807a8667f13d64
eurico77 <euricomagalhaes61@gmail.com>
2021-05-11 01:24:51 -0300

    adjustments

--- src/database/migrations/1620615606123-CreateUsers.ts
+++ src/database/migrations/1620615606123-CreateUsers.ts
@@ -9,40 +9,40 @@ export class CreateUsers1617836737832 implements MigrationInterface {
           {
             name: 'id',
             type: 'uuid',
-            isPrimary: true,
+            isPrimary: true
           },
           {
             name: 'name',
-            type: 'varchar',
+            type: 'varchar'
           },
           {
             name: 'username',
             type: 'varchar',
-            isUnique: true,
+            isUnique: true
           },
           {
             name: 'password',
-            type: 'varchar',
+            type: 'varchar'
           },
           {
             name: 'email',
-            type: 'varchar',
+            type: 'varchar'
           },
           {
             name: 'driver_license',
-            type: 'varchar',
+            type: 'varchar'
           },
           {
             name: 'isAdmin',
             type: 'boolean',
-            default: 'false',
+            default: 'false'
           },
           {
             name: 'created_at',
             type: 'timestamp',
-            default: 'now()',
-          },
-        ],
+            default: 'now()'
+          }
+        ]
       })
     );
   }
