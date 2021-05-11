743c3f2021f35d49307c40c6bb3e9803bbab5556
eurico77 <euricomagalhaes61@gmail.com>
2021-05-11 00:44:51 -0300

    feature: add autheticaded

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,28 @@
+import { NextFunction, Request, Response } from 'express';
+import { verify } from 'jsonwebtoken';
+
+import { UsersRepository } from '../modules/accounts/repositories/implementations/UserRepository';
+import { AppError } from '../shared/errors/AppError';
+
+interface IPayload {
+  sub: string;
+}
+
+export async function ensureAutenticated(request: Request, response: Response, next: NextFunction): Promise<void> {
+  const authHeader = request.headers.authorization;
+  if (!authHeader) throw new AppError('Token missing', 400);
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const { sub: user_id } = verify(token, '44f7e546e7154e04d7e9f13574be041c') as IPayload;
+    const usersRespository = new UsersRepository();
+    const user = await usersRespository.findById(user_id);
+
+    if (!user) throw new AppError('User does not exists!', 400);
+
+    next();
+  } catch {
+    throw new AppError('Token missing', 400);
+  }
+}
