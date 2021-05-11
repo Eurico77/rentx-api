b1f0b6efae4edd841a67345a8a971bdc7316659f
eurico77 <euricomagalhaes61@gmail.com>
2021-05-11 01:25:30 -0300

    feature add: usecase avatar

---
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -0,0 +1,30 @@
+import { inject, injectable } from 'tsyringe';
+
+import { deleteFile } from '../../../../utils/files';
+import { IUsersRepository } from '../../repositories/IUserRepository';
+
+interface IRequest {
+  user_id: string;
+  avatar_file: string;
+}
+
+@injectable()
+class UpdateUserAvatarUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({ user_id, avatar_file }: IRequest): Promise<void> {
+    const user = await this.usersRepository.findById(user_id);
+
+    if (user.avatar) {
+      await deleteFile(`./tmp/avatar/${user.avatar}`);
+    }
+
+    user.avatar = avatar_file;
+    await this.usersRepository.create(user);
+  }
+}
+
+export { UpdateUserAvatarUseCase };
